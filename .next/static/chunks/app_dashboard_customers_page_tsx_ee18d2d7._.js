fdeeb84c586e2a49a7eda247458774a32fdb7416
jaydipdarji9094 <jaydip.bliss@gmail.com>
2025-05-29 11:03:52 +0530

    manage gitignore file

--- .next/static/chunks/app_dashboard_customers_page_tsx_ee18d2d7._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_dashboard_customers_page_tsx_d008d749._.js",
-  "static/chunks/f07ee_next_dist_compiled_react_34c72dfa._.js",
-  "static/chunks/app_globals_73c37791.css"
-],
-    source: "dynamic"
-});
